462e5752a4c36d467b45414f594a6e0c4939a07e
iamsourav37 <souravganguly108@gmail.com>
2021-03-13 08:05:57 +0530

    get location method refactor

--- lib/screens/LoadingScreen.dart
+++ lib/screens/LoadingScreen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:weather_app/services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -8,10 +8,10 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
   void getLocation() async {
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high);
-
-    print("Location : $position");
+    Location location = Location();
+    await location.getCuttentPosition();
+    print(location.latitude);
+    print(location.longitude);
   }
 
   @override
